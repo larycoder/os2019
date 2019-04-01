985ff84604067f14a0c735df3af2893621bc4a83
larycoder <lenhuchuhiep99@gmail.com>
2019-04-01 11:14:02 +0700

    update autoText script

--- autoWriteText.sh
+++ autoWriteText.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-echo "$@: ">>01.report.report.shell.commands.dm
-eval "$@|head -n 10">>01.report.report.shell.commands.dm
+echo "$@: ">>01.report.shell.commands.txt
+eval "$@|head -n 10">>01.report.shell.commands.txt
+echo "--------------------">>01.report.shell.commands.txt
